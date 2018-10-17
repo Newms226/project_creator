3b84dc7a15c7244cacf69025c7e652f9b951fd11
Michael Newman <newms226@gmail.com>
2018-10-16 18:02:04 -0600

    License & hierarchy work

--- design/hierarchy_parsing.rst
+++ design/hierarchy_parsing.rst
@@ -1,11 +1,15 @@
 * Recursively looping through tree (with curdir set to the current dir)
     #. BASE CASE:
-        - If no children, return up recursive call
+        - If no children, return up recursive call (pop from stack)
     #. dir or file?
         - dir
             * generate temp directory
             * parse git strategy
+            * store curdir (push to stack)
+            * append this dir to the curdir
             * recursive call
+            * restore curdir (pop from stack)
+
         - file
             * generate valid file name by combining the extension and the name
             * (optional) add header
--- src/models/model_parser.py
+++ src/models/model_parser.py
@@ -4,20 +4,6 @@ import xml.etree.ElementTree.ParseError as ParseError
 
 class Tree(object):
     """Simple class to provide access to the XML tree
-
-    Attributes:
-        xml_config (str): the absolute path to the XML config file
-        tree: the ElementTree as parsed by xml.etree.ElementTree
-        root: the root of the ElementTree as parsed by
-            xml.etree.ElementTree.getroot()
-
-    Shortcuts:
-        info
-        files
-        folders
-        autofiles
-        git
-        sync
     """
 
     def __init__(self, xml_config):
@@ -29,15 +15,28 @@ class Tree(object):
 
         self.xml_config = xml_config
         self.tree = ET.parse(xml_config)
-        self.root = self.tree.getroot()
 
         try:
             self.info = self.tree.find('info')
-            self.files = self.tree.find('files')
-            self.folders = self.tree.find('folders')
-            self.autofiles = self.tree.find('autofiles')
             self.git = self.tree.find('git')
             self.sync = self.tree.find('sync')
+            self.root = self.tree.find('root')
+            # TODO ensure that all values are not null!
+
         except ParseError as err:
             print('Failed to parse: {0}'.format(err))
             raise err  # TODO error handling
+
+        self.setup = {}
+
+    def parse_info(self):
+        self.setup['name'] = self.info.get('name').text
+
+        contributors = []
+        for contributor in self.info.get('contributors'):
+            contributors.append(contributor)
+
+        self.setup['contributors'] = contributors
+        self.setup['rootdir'] = self.info.get('rootdir')
+        self.setup['license'] = self.info.get('license')
+        self.setup['date'] = self.info.get('date')
